faa8a68da203ff038d50324ffd6c24a5d5742b1c
Javier P <javierp@neftyblocks.com>
2022-08-18 16:48:26 +0200

    Update theme, add information banners

--- ckanext/demo/plugin.py
+++ ckanext/demo/plugin.py
@@ -1,9 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+def get_groups():
+    groups = toolkit.get_action('group_list')(data_dict={ 'all_fields': True })
+    return groups
 
 class DemoPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -12,3 +16,10 @@ class DemoPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'demo')
+
+    #ITemplateHelpers
+    def get_helpers(self):
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return { 'demo_groups': get_groups }
